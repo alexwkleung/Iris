e19911d532ec0d687be1cb899843f2437751fc37
Alex Leung <alexleung25@outlook.com>
2023-02-24 21:24:33 -0800

    Make codemirror editorview function return EditorView instead of void

--- src/codemirror/cm_editor_view/cm_editor_view.ts
+++ src/codemirror/cm_editor_view/cm_editor_view.ts
@@ -10,14 +10,28 @@ import { CodeMirror_EditorState } from '../cm_editor_state/cm_editor_state'
 
 export class CodeMirror_EditorView {
     //ref
+    /**
+     * editorView variable
+     * 
+     * Used as a reference variable when creating the CodeMirror EditorView
+     * @member static
+     * @returns EditorView
+     */
     static editorView: EditorView;
 
     //CodeMirror Editor View
-    public CodeMirror_EditorView() {
+    /**
+     * CodeMirror EditorView function
+     * 
+     * @returns EditorView
+     */
+    public CodeMirror_EditorView(): EditorView {
         CodeMirror_EditorView.editorView = new EditorView({
             state: CodeMirror_EditorState.CodeMirror_EditorState(),
             doc: "",
             parent: document.querySelector('#cm_editor') as HTMLElement
         });
+
+        return CodeMirror_EditorView.editorView;
     }
 }
