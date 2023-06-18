e53da2555ce6ab8ff116606c08476f8e009dc1ac
Alex Leung <alexleung25@outlook.com>
2023-06-18 13:53:22 -0700

    Added null checks

--- src/renderer/src/event-listeners/file-directory-state-listener.ts
+++ src/renderer/src/event-listeners/file-directory-state-listener.ts
@@ -6,27 +6,33 @@ export class DirectoryTreeStateListeners {
      */
     public activeChildFileStateListener(): void {
         document.querySelectorAll('.parent-folder-name').forEach((el) => {
-            el.addEventListener('click', () => {
-                //doc title folder name
-                //trim any whitespace from the split if necessary
-                const docTitleFolderName: string = document.title.split('-')[1].trim();
-
-                const childFileName: Element[] = Array.from(document.getElementsByClassName('child-file-name'));
-                
-                for(let i = 0; i < childFileName.length; i++) {
-                    //check if docTitleFolderName and docTitleFileName match the respective refs
-                    if(docTitleFolderName === RefsNs.currentParentChildData[0].parentFolderName && childFileName[i].textContent === RefsNs.currentParentChildData[0].childFileName) {                        
-                        console.log(RefsNs.currentParentChildData[0].childFileNode.textContent);
-                        //compare text content to make sure it's identical
-                        if(childFileName[i].textContent === RefsNs.currentParentChildData[0].childFileName) {
-                            //add is-active-child class to the child file 
-                            childFileName[i].classList.add('is-active-child');
-                        } else {
-                            continue;
+            //null check
+            if(el !== null) {
+                el.addEventListener('click', () => {
+                    //doc title folder name
+                    //trim any whitespace from the split if necessary
+                    const docTitleFolderName: string = document.title.split('-')[1].trim();
+    
+                    const childFileName: Element[] | null = Array.from(document.getElementsByClassName('child-file-name'));
+                    
+                    //null check
+                    if(childFileName !== null) {
+                        for(let i = 0; i < childFileName.length; i++) {
+                            //check if docTitleFolderName and docTitleFileName match the respective refs
+                            if(docTitleFolderName === RefsNs.currentParentChildData[0].parentFolderName && childFileName[i].textContent === RefsNs.currentParentChildData[0].childFileName) {                        
+                                console.log(RefsNs.currentParentChildData[0].childFileNode.textContent);
+                                //compare text content to make sure it's identical
+                                if(childFileName[i].textContent === RefsNs.currentParentChildData[0].childFileName) {
+                                    //add is-active-child class to the child file 
+                                    childFileName[i].classList.add('is-active-child');
+                                } else {
+                                    continue;
+                                }
+                            }
                         }
                     }
-                }
-            });
+                });
+            }
         });
     }
 }
