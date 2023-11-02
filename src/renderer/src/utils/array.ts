5912033db182e68b48e5e0f326c522b506c7e906
Alex Leung <alexleung25@outlook.com>
2023-11-01 17:59:36 -0700

    Update empty function to take in generic types

--- src/renderer/src/utils/array.ts
+++ src/renderer/src/utils/array.ts
@@ -6,25 +6,25 @@ export namespace GenericArray {
     /**
      * @internal
      */
-    class Array implements IArray<any, boolean, number> {
+    class Array implements IArray<any, boolean, number | any[]> {
         /**
          * 
          * @param arr Array to empty
          * @param pop Option to empty array by popping the stack instead of setting length
          * @returns Emptied array
          */
-        public empty(arr: any[], pop: boolean): number {
-            let empty: number = 0;
+        public empty<T, K, V>(arr: T[], pop: K): V {
+            let empty: number | any[] = {} as number | any[];
 
             if(!pop && (arr.length !== 0 && arr.length !== -1 || arr.length !== undefined || arr.length !== null)) {
                 empty = arr.length = 0;
             } else if(pop) {
                 while(arr.length > 0) {
-                    empty = arr.pop();
+                    empty = arr.pop() as any[];
                 }
             }
 
-            return empty;
+            return empty as V;
         }
     }
 
