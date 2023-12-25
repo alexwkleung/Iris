e235d76205916315b575b21064142d01f5909536
Alex Leung <alexleung25@outlook.com>
2023-12-24 19:23:27 -0800

    Fix duplicate file count elements in dom

--- src/renderer/misc-ui/folder-file-count.ts
+++ src/renderer/misc-ui/folder-file-count.ts
@@ -11,56 +11,33 @@ export class FolderFileCount {
      * @param parentNameTagsArr Elements of `parentNameTagsArr()`
      * @param reset Option to reset file count
      */
-    public folderFileCount(parentNameTags: Element, parentNameTagsArr: string, reset: boolean): void {
-        let fileCount: number = 0;
-
-        if (reset) {
-            console.log("reset");
-
-            //reset count to 0
-            fileCount = 0;
-
-            //folder file count container
-            const folderFileCountContainer: HTMLDivElement = document.createElement("div");
-            folderFileCountContainer.setAttribute("class", "folder-file-count-container");
-            parentNameTags.insertBefore(folderFileCountContainer, parentNameTags.firstChild);
-
-            //assign length of walked folder (excluding itself) to fileCount
-            //eslint-disable-next-line
-            //@ts-ignore
-            if (window.electron.process.platform === "darwin") {
-                fileCount = fsMod.fs
-                    ._walk(fsMod.fs._baseDir("home") + "/Iris/Notes" + "/" + parentNameTagsArr)
-                    .slice(1).length;
-            } else {
-                fileCount = fsMod.fs._walk(fsMod.fs._baseDir("home") + "/Iris/Notes" + "/" + parentNameTagsArr).length;
-            }
-
-            //folder file count text node
-            const folderFileCountTextNode: Text = document.createTextNode(fileCount.toString());
-            folderFileCountContainer.appendChild(folderFileCountTextNode);
-        } else if (!reset) {
-            console.log("not reset");
-
-            //folder file count container
-            const folderFileCountContainer: HTMLDivElement = document.createElement("div");
-            folderFileCountContainer.setAttribute("class", "folder-file-count-container");
-            parentNameTags.insertBefore(folderFileCountContainer, parentNameTags.firstChild);
-
-            //assign length of walked folder (excluding itself) to fileCount
-            //eslint-disable-next-line
-            //@ts-ignore
-            if (window.electron.process.platform === "darwin") {
-                fileCount = fsMod.fs
-                    ._walk(fsMod.fs._baseDir("home") + "/Iris/Notes" + "/" + parentNameTagsArr)
-                    .slice(1).length;
-            } else {
-                fileCount = fsMod.fs._walk(fsMod.fs._baseDir("home") + "/Iris/Notes" + "/" + parentNameTagsArr).length;
-            }
-
-            //folder file count text node
-            const folderFileCountTextNode: Text = document.createTextNode(fileCount.toString());
-            folderFileCountContainer.appendChild(folderFileCountTextNode);
+    public folderFileCount(parentNameTags: Element, parentNameTagsArr: string): void {
+        //check existing
+        if ((document.querySelector(".folder-file-count-container") as HTMLElement) !== null) {
+            (document.querySelector(".folder-file-count-container") as HTMLElement).remove();
         }
+
+        let fileCount: number = 0;
+        console.log("reset");
+
+        //reset count to 0
+        fileCount = 0;
+
+        //folder file count container
+        const folderFileCountContainer: HTMLDivElement = document.createElement("div");
+        folderFileCountContainer.setAttribute("class", "folder-file-count-container");
+        parentNameTags.insertBefore(folderFileCountContainer, parentNameTags.firstChild);
+
+        if (window.electron.process.platform === "darwin") {
+            fileCount = fsMod.fs
+                ._walk(fsMod.fs._baseDir("home") + "/Iris/Notes" + "/" + parentNameTagsArr)
+                .slice(1).length;
+        } else {
+            fileCount = fsMod.fs._walk(fsMod.fs._baseDir("home") + "/Iris/Notes" + "/" + parentNameTagsArr).length;
+        }
+
+        //folder file count text node
+        const folderFileCountTextNode: Text = document.createTextNode(fileCount.toString());
+        folderFileCountContainer.appendChild(folderFileCountTextNode);
     }
 }
