e160d092905f5c5090057a231437612c6096726c
Alex Leung <alexleung25@outlook.com>
2024-01-01 22:41:21 -0800

    Update word count

--- src/renderer/event-listeners/word-count-listener.ts
+++ src/renderer/event-listeners/word-count-listener.ts
@@ -4,76 +4,63 @@ import { CMEditorView } from "../codemirror/editor/cm-editor-view";
 import { TextIterator } from "@codemirror/state";
 import { GenericEvent } from "./event";
 
-/**
- * Word count listener
- *
- * @param editor Editor type (options: `"prosemirror"`, `"codemirror"`)
- * @returns Word count
- */
-export function wordCountListener(editor: string): number {
-    //default value
-    let wordCount: number = 0;
+class WordCount {
+    public static count: number = 0;
 
-    if (editor === "codemirror") {
-        const cm: HTMLElement = document.querySelector(".cm-editor") as HTMLElement;
-
-        //show word count
-        WordCountContainerNode.wordCountContainer.style.display = "";
-
-        WordCountContainerNode.wordCountContainer.textContent = wordCount.toString() + " words";
-
-        //reset word count
-        wordCount = 0;
-
+    public static createWordCount(): void {
         //taken from: https://codemirror.net/examples/panel/
         const iter: TextIterator = CMEditorView.editorView.state.doc.iter();
         while (!iter.next().done) {
             let inWord: boolean = false;
 
             for (let i = 0; i < iter.value.length; i++) {
-                const word: boolean = /[\w]/.test(iter.value[i]);
+                //https://stackoverflow.com/questions/4593565/regular-expression-for-accurate-word-count-using-javascript
+                const word: boolean = /[/\b\S+\b/g]/.test(iter.value[i]);
 
                 if (word && !inWord) {
-                    wordCount++;
+                    WordCount.count++;
                 }
 
                 inWord = word;
             }
         }
 
-        //initial word count
-        WordCountContainerNode.wordCountContainer.textContent = wordCount.toString() + " words";
+        if (!WordCount.count) {
+            WordCountContainerNode.wordCountContainer.textContent = WordCount.count.toString() + " words";
+        } else {
+            WordCountContainerNode.wordCountContainer.textContent = WordCount.count.toString() + " words";
+        }
+    }
+}
+/**
+ * Word count listener
+ *
+ * @param editor Editor type (options: `"codemirror"`)
+ * @returns Word count
+ */
+export function wordCountListener(editor: string): number {
+    if (editor === "codemirror") {
+        const cm: HTMLElement = document.querySelector(".cm-editor") as HTMLElement;
 
-        console.log(wordCount);
+        //show word count
+        WordCountContainerNode.wordCountContainer.style.display = "";
+        WordCountContainerNode.wordCountContainer.textContent = WordCount.count.toString() + " words";
+
+        //reset word count
+        WordCount.count = 0;
+
+        WordCount.createWordCount();
+
+        //initial word count
+        WordCountContainerNode.wordCountContainer.textContent = WordCount.count.toString() + " words";
 
         const cmDebounceWordCount: () => any = debounce(() => {
             console.log(cm.textContent);
 
             //reset word count
-            wordCount = 0;
+            WordCount.count = 0;
 
-            const iter: TextIterator = CMEditorView.editorView.state.doc.iter();
-            while (!iter.next().done) {
-                let inWord: boolean = false;
-
-                for (let i = 0; i < iter.value.length; i++) {
-                    const word: boolean = /[\w]/.test(iter.value[i]);
-
-                    if (word && !inWord) {
-                        wordCount++;
-                    }
-
-                    inWord = word;
-                }
-            }
-
-            if (!wordCount) {
-                WordCountContainerNode.wordCountContainer.textContent = wordCount.toString() + " words";
-            } else {
-                WordCountContainerNode.wordCountContainer.textContent = wordCount.toString() + " words";
-            }
-
-            console.log(wordCount);
+            WordCount.createWordCount();
         }, 250); //250ms default
 
         GenericEvent.use.createDisposableEvent(
@@ -85,5 +72,5 @@ export function wordCountListener(editor: string): number {
         );
     }
 
-    return wordCount;
+    return WordCount.count;
 }
